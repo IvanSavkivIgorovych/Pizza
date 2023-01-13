8e8dfa2aeb47bd12d0763f637bead4dcf8404a40
Ivan <ivan.savkiv.igorovych@gmail.com>
2023-01-14 00:28:51 +0200

    Add some components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Сlosed</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
